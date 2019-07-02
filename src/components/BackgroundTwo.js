697f1790b6ef20220f08c397216d57be98425082
Code on Point <booker-t-8@Codes-MacBook-Air.local>
2019-07-02 11:39:38 -0700

    Modified background 2

--- src/components/BackgroundTwo.js
+++ src/components/BackgroundTwo.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const BackgroundTwo = () => (
   <div id="background-two">
-
+  <div className="bg"></div>
+<div className="bg bg2"></div>
+<div className="bg bg3"></div>
+<div className="content">
+  <h1>Useful Resources</h1>
+</div>
   </div>
 )
 export default BackgroundTwo;
--- src/components/aboutCard.js
+++ src/components/aboutCard.js
@@ -4,9 +4,9 @@ export default class AboutCard extends Component {
   constructor(props){
     super(props);
     this.state ={
-      image: "https://www.w3schools.com/w3css/img_lights.jpg",
-      title: "Adrian",
-      description: "This is mi site"
+      image: "../images/w3schools.png",
+      title: "W3SchoolS",
+      description: "W3Schools is an educational website for learning web technologies online. Content includes tutorials and references relating to HTML, CSS, JavaScript, JSON, PHP, Python, AngularJS, SQL, Bootstrap, Node.js, jQuery, XQuery, AJAX, XML, and Java."
     }
   }
   render(){
@@ -17,7 +17,7 @@ export default class AboutCard extends Component {
         <div className="card-body">
           <h5 className="card-title"> { title } </h5>
           <p className="card-text"> { description } </p>
-          <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+          <p className="card-text"><small className="text-muted"><a href="https://www.w3schools.com/" target="_blank">Web site</a></small></p>
         </div>
       </div>
     )
