48784caba7898d6b3343c6e946047386ec907f59
Code on Point <booker-t-8@Codes-MacBook-Air.local>
2019-07-02 12:20:00 -0700

    Modified background 3

--- src/components/BackgroundThree.js
+++ src/components/BackgroundThree.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const BackgroundThree = () => (
-  <div id="background-three">
-
+  <div id="background-three" className="title">
+     <h1 className="fontEffect">thanks<br/>for watching</h1>
   </div>
 )
 export default BackgroundThree;
